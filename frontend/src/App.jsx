b76403cfc70116e45173c1f018dac613082245a7
harshalkotkar156 <harshalkotkar156@gmail.com>
2026-02-01 23:37:48 +0530

    Navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,18 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Route,Routes} from "react-router-dom";
+import Home from './pages/Home';
+import Navbar from './components/common/Navbar';
 import './App.css'
 
 function App() {
-  const [flag1,setFlag1] = useState(true);
-  const [flag2,setFlag2] = useState(false);
-
-  useEffect(() => {
-    
-  },[flag1,flag2])
-
-  
 
   return (
-    <>
-
-      {
-        flag1 ? (
-          <div>
-            {
-              flag2 ? (
-                <div>
-                    <button onClick={() => {setFlag2(false)}}>inside If </button>
-                  </div>
-              ) : 
-              (
-                  <div>
-                    <button onClick={() => {setFlag2(true)}}>Inside else</button>
-                  </div>
-              )
-            } 
-
-          </div>
-        ) : 
-        (
-          <div >
-            <button>Main else</button>
-          </div>
-        )
-
-
-      }
-      
-        
-    </>
+    <div className='w-screen min-h-screen bg-richblack-900 flex-col font-inter '>
+        <Navbar/>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </div>
   )
 }
 
