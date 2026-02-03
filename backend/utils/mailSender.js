32c83cf5560a94e3fd97afa76537d49c26398795
harshalkotkar156 <harshalkotkar156@gmail.com>
2026-02-03 23:26:19 +0530

    resend api added in Maildender

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -1,14 +1,15 @@
 const nodemailer = require('nodemailer');
+const { Resend } = require("resend");
 
-const mailSender = async (email,title,body) => {
+const mailSender= async (email, title, body) => {
 
-    
+
     try {
-       
+
         const transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
             port: 587,
-            secure: false, 
+            secure: false,
             requireTLS: true,
             logger: true,
             debug: true,
@@ -20,18 +21,44 @@ const mailSender = async (email,title,body) => {
         });
 
         let info = await transporter.sendMail({
-            from:"Study-Notion",
-            to:email,
-            subject:title,
-            html :body
+            from: "Study-Notion",
+            to: email,
+            subject: title,
+            html: body
         })
 
-        
-        return info;       
+
+        return info;
     } catch (error) {
-        console.log("error in sending mail",error);
+        console.log("error in sending mail", error);
     }
 
 }
 
+
+
+
+const resend = new Resend(process.env.RESEND_EMAIL_API_KEY);
+
+const mailSenderAPI = async (email, title, body) => {
+    try {
+        
+        const { data, error } = await resend.emails.send({
+            from:process.env.MAIL_SENDER,
+            to: 'harshalkotkar1713@gmail.com',
+            subject: title,
+            html: `<strong>OTP : ${body}</strong>`,
+        });
+
+        if (error) {
+            return console.error({ error });
+        }
+
+        console.log({ data });
+    } catch (error) {
+        console.log("Error is : ",error);
+    }
+}
+
 module.exports = mailSender;
+
