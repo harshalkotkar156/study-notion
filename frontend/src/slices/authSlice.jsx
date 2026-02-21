cfe513f8b1e211d1f4c8d761948c9a5853e618e8
harshalkotkar156 <harshalkotkar156@gmail.com>
2026-02-21 10:26:30 +0530

    required Slices added

--- frontend/src/slices/authSlice.jsx
+++ frontend/src/slices/authSlice.jsx
@@ -1,28 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   token: localStorage.getItem("token") || null,
-// };
-
-
-// const authSlice = createSlice({
-//     name:"auth",
-//     initialState:initialState,
-//     reducers:{
-//         setSignupData(state, value) {
-//           state.signupData = value.payload;
-//         },
-//         setLoading(state, value) {
-//             state.loading = value.payload;
-//         },
-//         setToken(state,token){
-//             state.token = value.payload;
-//         }
-//     }
-// })
-
-// export const {setToken,setSignupData,setLoading} = authSlice.actions;
-// export default authSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
