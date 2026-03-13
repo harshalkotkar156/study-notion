824f2efdcc08a822328a931f7104c75e88af2bc5
harshalkotkar156 <harshalkotkar156@gmail.com>
2026-03-13 23:53:11 +0530

    profile updateDisplay added

--- backend/routes/Profile.js
+++ backend/routes/Profile.js
@@ -5,13 +5,16 @@ const {updateProfile,
     deleteAccount,
     getUserDetails,
     instructorDashboard,
-    getEnrolledCourses} = require("../controllers/Profile");
+    getEnrolledCourses,
+    updateDisplayPicture,
+    } = require("../controllers/Profile");
 
 const {auth,isInstructor} = require("../middleware/auth");
+// const { updateDisplayPicture } = require("../../frontend/src/services/operations/SettingAPI");
 router.put('/update-profile' ,auth, updateProfile);
 router.delete('/delete-profile' ,auth, deleteAccount);
 router.get('/get-user-details' ,auth, getUserDetails);
 router.get('/instructor-dashboard' ,auth,isInstructor, instructorDashboard);
 router.get('/enrolled-courses' ,auth, getEnrolledCourses);
-
+router.put("/update-display-picture",auth,updateDisplayPicture);
 module.exports=router;
